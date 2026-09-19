31b31a3a132226a957b329556e9a277cc04d9ffa
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 10:22:13 +0100

    Initial Commit!

---
+++ src/ci3/date_lang.php
@@ -0,0 +1,75 @@
+<?php
+
+$lang['date_year'] = 'Година';
+$lang['date_years'] = 'Години';
+$lang['date_month'] = 'Месец';
+$lang['date_months'] = 'Месеци';
+$lang['date_week'] = 'Седмица';
+$lang['date_weeks'] = 'Седмици';
+$lang['date_day'] = 'Ден';
+$lang['date_days'] = 'Дни';
+$lang['date_hour'] = 'Час';
+$lang['date_hours'] = 'Часа';
+$lang['date_minute'] = 'Минута';
+$lang['date_minutes'] = 'Минути';
+$lang['date_second'] = 'Секунда';
+$lang['date_seconds'] = 'Секунди';
+
+$lang['date_just_now'] = 'точно сега';
+$lang['date_seconds_ago'] = '=1{преди # секунда} other{преди # секунди}';
+$lang['date_minutes_ago'] = '=1{преди # минута} other{преди # минути}';
+$lang['date_hours_ago'] = '=1{преди # час} other{преди # часа}';
+$lang['date_days_ago'] = '=1{вчера} other{преди # дни}';
+$lang['date_weeks_ago'] = '=1{преди # седмица} other{преди # седмици}';
+$lang['date_months_ago'] = '=1{преди # месец} other{преди # месеца}';
+$lang['date_years_ago'] = '=1{преди # година} other{преди # години}';
+
+$lang['date_su'] = 'Нд';
+$lang['date_mo'] = 'Пн';
+$lang['date_tu'] = 'Вт';
+$lang['date_we'] = 'Ср';
+$lang['date_th'] = 'Чт';
+$lang['date_fr'] = 'Пт';
+$lang['date_sa'] = 'Сб';
+
+$lang['date_sun'] = 'Нед';
+$lang['date_mon'] = 'Пон';
+$lang['date_tue'] = 'Вто';
+$lang['date_wed'] = 'Сря';
+$lang['date_thu'] = 'Чет';
+$lang['date_fri'] = 'Пет';
+$lang['date_sat'] = 'Съб';
+
+$lang['date_sunday'] = 'Неделя';
+$lang['date_monday'] = 'Понеделник';
+$lang['date_tuesday'] = 'Вторник';
+$lang['date_wednesday'] = 'Сряда';
+$lang['date_thursday'] = 'Четвъртък';
+$lang['date_friday'] = 'Петък';
+$lang['date_saturday'] = 'Събота';
+
+$lang['date_jan'] = 'Яну';
+$lang['date_feb'] = 'Фев';
+$lang['date_mar'] = 'Мар';
+$lang['date_apr'] = 'Апр';
+$lang['date_may'] = 'Май';
+$lang['date_jun'] = 'Юни';
+$lang['date_jul'] = 'Юли';
+$lang['date_aug'] = 'Авг';
+$lang['date_sep'] = 'Сеп';
+$lang['date_oct'] = 'Окт';
+$lang['date_nov'] = 'Ное';
+$lang['date_dec'] = 'Дек';
+
+$lang['date_january'] = 'Януари';
+$lang['date_february'] = 'Февруари';
+$lang['date_march'] = 'Март';
+$lang['date_april'] = 'Април';
+$lang['date_mayl'] = 'Май';
+$lang['date_june'] = 'Юни';
+$lang['date_july'] = 'Юли';
+$lang['date_august'] = 'Август';
+$lang['date_september'] = 'Септември';
+$lang['date_october'] = 'Октомври';
+$lang['date_november'] = 'Ноември';
+$lang['date_december'] = 'Декември';
